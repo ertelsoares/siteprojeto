f2be981c6e71771c43ddfdc4ee3b4db3d380ffa4
Lucas Ertel <54690363+ertelsoares@users.noreply.github.com>
2023-06-30 21:05:51 -0300

    Alterado arquivo

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+
 // 
 // Meio de usar em uma tag
-
 // const About =  () => <h1>About</h1>
-
-
 // export default About;
+//Função abaixo utilizando o react Fragment
+const About = () =>{
+    <>
+    <h1>About </h1>
+    <h2>About 2</h2>
+    </>
+}
+
+export default About;
