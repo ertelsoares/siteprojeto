9693a6630d9fa6676e2bc78287c8b30aaf00a7aa
Lucas Ertel <54690363+ertelsoares@users.noreply.github.com>
2023-06-30 21:35:18 -0300

    alteração e componente

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import Menu from '../components/Menu';
 
 
-// 
 // Meio de usar em uma tag
 // const About =  () => <h1>About</h1>
 // export default About;
 //Função abaixo utilizando o react Fragment
+// 
+// Função sem return
+// const About = () =>(
+// //     <>
+// //     <h1>About </h1>
+// //     <h2>About 2</h2>
+// //     </>
+// // )
 const About = () =>{
+    const text = 'meu Texto'
+    return(
     <>
+    <Menu />
     <h1>About </h1>
-    <h2>About 2</h2>
+    <h2>{text}</h2>
     </>
+   )
 }
 
 export default About;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Menu from '../components/Menu';
+
+const Contact = () =>{
+    const text = 'meu Texto'
+    return(
+    <>
+     <Menu />
+    <h1>Contact</h1>
+    <h2>{text}</h2>
+    </>
+   )
+}
+
+export default Contact;
